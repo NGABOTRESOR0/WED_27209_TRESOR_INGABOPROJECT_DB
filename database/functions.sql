f1c5b7494e152c1090b23833724b4698250e3bba
NGABOTRESOR0 <ngabotresor226@gmail.com>
2025-12-09 16:00:02 +0200

    Update functions.sql

--- database/functions.sql
+++ database/functions.sql
@@ -1,1 +1,21 @@
+CREATE OR REPLACE FUNCTION check_restriction RETURN VARCHAR2 IS
+  v_today DATE := TRUNC(SYSDATE);
+  v_day VARCHAR2(10);
+  v_count NUMBER;
+BEGIN
+  v_day := TO_CHAR(v_today, 'DY', 'NLS_DATE_LANGUAGE=EN');
 
+  -- Deny on weekends
+  IF v_day IN ('SAT', 'SUN') THEN
+    RETURN 'WEEKEND';
+  END IF;
+
+  -- Deny on holidays
+  SELECT COUNT(*) INTO v_count FROM holidays WHERE holiday_date = v_today;
+  IF v_count > 0 THEN
+    RETURN 'HOLIDAY';
+  END IF;
+
+  RETURN 'ALLOWED';
+END;
+/
